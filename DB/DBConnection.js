b08402855542de0a10616952f69df85bde67629b
amirafa59 <amirafa592001@gmail.com>
2025-10-18 18:16:00 +0300

    db commit

--- DB/DBConnection.js
+++ DB/DBConnection.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 
 const dbConnection = () => {
-    // mongoose.connect("mongodb+srv://fathyamira689:amiraFGMongoAtlas159@cluster0.9yhzmr9.mongodb.net/ecommerce")// DB Link
-   // mongodb+srv://fathyamira689:<db_password>@cluster0.lcr45o7.mongodb.net/
-   // mongodb+srv://fathyamira689:amiraFGMongoAtlas159@cluster0.lcr45o7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0.lcr45o7.mongodb.net/Task1
-   // mongoose.connect("mongodb+srv://backbenchersteam23:cZMciniOgAaEU45q@cluster0.q4aihf4.mongodb.net/ecommerce?retryWrites=true&w=majority")// DB Link
-    mongoose.connect("mongodb+srv://fathyamira689:amiraFGMongoAtlas159@cluster0.lcr45o7.mongodb.net/Task1Backend?retryWrites=true&w=majority")// DB Link
+    mongoose.connect(process.env.DB)// DB Link
 
         .then(conn => {console.log(`Database connected on ${process.env.DB}`)
         
